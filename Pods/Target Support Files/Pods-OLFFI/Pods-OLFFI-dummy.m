203a2599450953ebb8b34e6ccff2b2cde94dfc84
graphee-gabriel <gabriel@graph.ee>
2017-01-08 16:41:19 +0200

    Swift 3.0

---
+++ Pods/Target Support Files/Pods-OLFFI/Pods-OLFFI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OLFFI : NSObject
+@end
+@implementation PodsDummy_Pods_OLFFI
+@end
---
+++ Pods/Target Support Files/Pods-OLFFITests/Pods-OLFFITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OLFFITests : NSObject
+@end
+@implementation PodsDummy_Pods_OLFFITests
+@end
